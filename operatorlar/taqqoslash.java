cc5864be2bad7d8c9e1793d2942ce856d45a4245
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-02-08 22:45:18 +0300

    Create taqqoslash.java

---
+++ operatorlar/taqqoslash.java
@@ -0,0 +1,19 @@
+public class taqqoslash {
+    public static void main(String[] args){
+        int a= 13;
+        int b=3;
+        boolean d= a>b;
+        System.out.println("a soni b sonidan kattami: "+d);
+        boolean c= a<b;
+        System.out.println("a soni b sonidan kichikmi: "+c);
+        boolean l= a>=b;
+        System.out.println("a soni b sonidan katta yoki tengmi: "+l);
+        boolean w= a<=b;
+        System.out.println("a soni b sonidan kichik yoki tengmi: "+w);
+        boolean u= a==b;
+        System.out.println("a va b soni tengmi: "+u);
+        boolean p= a!=b;
+        System.out.println("a va b soni tengmi: "+p);
+
+    }
+}
