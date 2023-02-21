fc3fe56b4fc495840c79e48f48718aedf0b5298b
DAVRONBEK ABDURAZZOQOV <94719744+21040001@users.noreply.github.com>
2023-02-21 12:38:21 +0300

    Create Hisoblash_v2.java

---
+++ Hisoblash_v2.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hisoblash_v2 {
+    public static  void  main(String[] args){
+        Random random = new Random();
+        double hisobim = random.nextInt(2000);
+        System.out.println("Hisobingizdagi pul miqdori = " + hisobim);
+
+        System.out.print("Hisobingizdan yechmoqchi bo'lgan pul miqdoringizni kiring : ");
+        Scanner scanner = new Scanner(System.in);
+        double yechiladigan_pul = scanner.nextDouble();
+
+        if (hisobim>yechiladigan_pul){
+            hisobim -= yechiladigan_pul;
+            System.out.println("Hisobingizdan yechib olingan pul miqdori = " + yechiladigan_pul);
+            System.out.println("Hisobingizda qolgan pul miqdori = " + hisobim);
+        }
+        else {
+            System.out.println("Hisobingizda yetarlicha pul miqdori mavjud emas!!");
+        }
+
+    }
+}
